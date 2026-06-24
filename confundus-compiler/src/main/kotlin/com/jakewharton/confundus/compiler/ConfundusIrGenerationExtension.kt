1caf1ecee15cfb211e195437eb32c67e22cf47b3
Jake Wharton <jw@jakewharton.com>
2026-06-24 11:17:50 -0400

    Remove unused MessageCollector (#131)
    It will be an error in Kotlin 2.4.20.

--- confundus-compiler/src/main/kotlin/com/jakewharton/confundus/compiler/ConfundusIrGenerationExtension.kt
+++ confundus-compiler/src/main/kotlin/com/jakewharton/confundus/compiler/ConfundusIrGenerationExtension.kt
@@ -2,13 +2,10 @@ package com.jakewharton.confundus.compiler
 
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
-import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 
-class ConfundusIrGenerationExtension(
-  private val messageCollector: MessageCollector,
-) : IrGenerationExtension {
+class ConfundusIrGenerationExtension : IrGenerationExtension {
   override fun generate(moduleFragment: IrModuleFragment, pluginContext: IrPluginContext) {
-    moduleFragment.transform(ConfundusElementTransformer(messageCollector, pluginContext), null)
+    moduleFragment.transform(ConfundusElementTransformer(pluginContext), null)
   }
 }
